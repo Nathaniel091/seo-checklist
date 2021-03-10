3ed72deb1e8847ea53a9e54d522da9a72d65bb3f
Nathaniel091 <nathaniel091@gmail.com>
2021-03-10 01:28:14 +0100

    fixed all SEO issues

--- main.js
+++
@@ -1,7 +0,0 @@
-
-function currentYear() {
-	const copyright = document.getElementById("copyright");
-	const currentYear = new Date().getFullYear();
-	copyright.textContent += ` ${currentYear}`;	
-}
-currentYear();
